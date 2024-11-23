1f73aae7fc7ebf508a09e56ff92700e5a463e6bd
Orii1 <farisansori231005@gmail.com>
2024-11-23 20:44:11 +0800

    add login with google

---
+++ app/Http/Controllers/GoogleController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Customer;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Str;
+
+class GoogleController extends Controller
+{
+    public function redirect()
+    {
+        return Socialite::driver('google')->redirect();
+    }
+
+    public function googleCallback(Request $request)
+    {
+        try {
+            $user = Socialite::driver('google')->user();
+
+            $existingUser = User::where('email', $user->getEmail())->first();
+
+            $user_mail = $user->getEmail();
+            $user_name = $user->getName();
+
+            if ($existingUser) {
+                Auth::login($existingUser);
+                return redirect('/');
+            } else {
+                return view('auth.register',compact('user_mail','user_name'));
+            }
+        } catch (\Exception $e) {
+            return redirect('/login');
+        }
+    }
+
+    public function register(Request $request)
+    {
+        $request->validate([
+            'number'     => 'required|numeric',
+            'address'  => 'required'
+        ]);
+
+        $user = User::create([
+            'email'  => $request->user_mail,
+            'name' => $request->user_name,
+            'remember_token' => Str::random(10),
+            'role_id' => 3
+        ]);
+
+        Customer::create([
+            'user_id' => $user->id,
+            'number' => $request->number,
+            'address' => $request->address,
+        ]);
+
+        Auth::login($user);
+        return redirect('/');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\GoogleController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -16,6 +17,10 @@ Route::middleware(['auth'])->group(function () {
 
 
 Route::middleware(['guest'])->group(function () {
+    Route::get('/auth/redirect', [GoogleController::class, 'redirect'])->name('google.redirect');
+    Route::get('/google/redirect', [GoogleController::class, 'googleCallback'])->name('google.callback');
+    Route::post('/register', [GoogleController::class, 'register'])->name('register');
+
     Route::get('/login', [AuthController::class, 'loginPage'])->name('login');
     Route::post('/login', [AuthController::class, 'login']);
 });
