eb1a616ec283c46d815ea0fdd156cf758db8b542
Orii1 <farisansori231005@gmail.com>
2024-11-23 22:40:58 +0800

    db rollback on create new user

--- app/Http/Controllers/GoogleController.php
+++ app/Http/Controllers/GoogleController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
 use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
 
 class GoogleController extends Controller
 {
@@ -38,26 +39,42 @@ class GoogleController extends Controller
     }
 
     public function register(Request $request)
-    {
-        $request->validate([
-            'number'     => 'required|numeric',
-            'address'  => 'required'
-        ]);
+{
+    $request->validate([
+        'number'    => 'required|numeric',
+        'address'   => 'required|string|max:255',
+    ]);
 
+    DB::beginTransaction();
+
+    try {
         $user = User::create([
-            'email'  => $request->user_mail,
-            'name' => $request->user_name,
+            'email'          => $request->user_mail,
+            'name'           => $request->user_name,
             'remember_token' => Str::random(10),
-            'role_id' => 3
+            'role_id'        => 3,
         ]);
 
-        Customer::create([
+        $customer = Customer::create([
             'user_id' => $user->id,
-            'number' => $request->number,
+            'number'  => $request->number,
             'address' => $request->address,
         ]);
 
-        Auth::login($user);
-        return redirect('/');
+        if ($user && $customer) {
+            DB::commit();
+            Auth::login($user);
+            return redirect('/')->with('success', 'Registration successful!');
+        }
+
+        throw new \Exception('Failed to create user or customer data.');
+
+    } catch (\Exception $e) {
+        DB::rollBack();
+
+        return redirect()->back()->withErrors([
+            'error' => 'Registration failed: ' . $e->getMessage(),
+        ]);
     }
+}
 }
